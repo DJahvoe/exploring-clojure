0cfc24d211270176de66ac3c25d0ceefabf59148
DJahvoe <dnaspeedmania@gmail.com>
2026-04-22 23:10:55 +0900

    Maps, Nested Map, Keywords, Vector, Lists

--- src/clojure-for-the-brave-and-true/chapter_3.clj
+++ src/clojure-for-the-brave-and-true/chapter_3.clj
@@ -59,7 +59,7 @@ error-message
 ;; Function with defn
 (defn error-message
   [age]
-  (str "RESULT: " 
+  (str "RESULT: "
        (if (>= age 20)
          "You're ALLOWED to enter"
          "You're NOT ALLOWED to enter")))
@@ -77,5 +77,58 @@ error-message
 "World"
 "\"It's Me! Mario!\""
 
-;; LAST PAGE: 43
+; Maps
+{}
+{:first-name "Sav"
+ :last-name "Test"}
+{"string-key" +}
+{:name {:first "Sav"
+        :last "Test"}}
+
+(def test-map (hash-map
+               :a 1
+               :b 2))
+(get test-map :b)
+(get test-map :c "Not Found")
+
+; Nested Map
+(def nested-map {:a 0
+                 :b {:c "hello world"}})
+(get-in nested-map [:b :c])
+
+; Keywords -> usually used as keys in maps
+; Equivalent for get
+(def test-keywords {:a 1 :b 2 :c 3})
+(:a test-keywords)
+(get test-keywords :a)
+
+(:d test-keywords "Not Found default")
+(get test-keywords :d "Not Found default")
+
+; Vector -> similar to array, each element can be assigned to any element
+(def test-vector [3 2 1])
+(get test-vector 0)
+
+; Vector created by function
+(def test-vector-fn (vector "this" "is" "an" "apple"))
+; conj to add element to the end of vector
+(conj test-vector-fn "hello")
+
+; Lists -> similar to vectors, but can't retrive list elements with get
+'(1 2 3 4)
+; nth function
+(def test-list '("hello" "world" "name's" "Sav"))
+(nth test-list 3)
+
+; list function
+(def test-list-fn (list 1 "two" {3, 4}))
+; conj add the beginning of list
+(conj test-list-fn 4)
+
+; When to use List, and When should we use Vector?
+; Rule of thumb: 
+; add items to the beginning of a sequence, OR writing a macro -> use List
+; Otherwise -> Vector
+
+;; LAST PAGE: 46 Sets
 
